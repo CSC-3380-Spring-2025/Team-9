7bd26daca9d3f940cd3d91d31da3f3482e6ff343
Diego Jose Albertini <146412355+Guinchi@users.noreply.github.com>
2025-03-03 20:31:17 -0600

    Update with pascalcase and error prevention CameraFollow.cs

--- Project Nine/Assets/Scripts/Player/CameraFollow.cs
+++ Project Nine/Assets/Scripts/Player/CameraFollow.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
-    public Transform target;
+    public Transform Target;
 
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        if (Target == null) return; // error prevention if target is unassigned
+    
+        Vector3 newPos = new Vector3(Target.position.x, Target.position.y, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
