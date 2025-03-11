045b1c2733ef8b6c1b6eb52935d38f630563bc95
Aedan <adefra5@lsu.edu>
2025-03-10 19:09:30 -0500

    {feat} Added Comments to the Walker class

--- Project Nine/Assets/Scripts/LevelGeneration/Walker.cs
+++ Project Nine/Assets/Scripts/LevelGeneration/Walker.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 
+/*
+ * The walker class is simply an object given three parameters to manipulate for the creation of rooms
+ */
 public class Walker
 {
     public Vector2 Position;
     public Vector2 Direction;
     public float chanceToChange;
 
+    /*
+     * An object of the class
+     * @param pos {Vector2} sets the position of the walker
+     * @param dir {Vector2} sets the initial direction of the walker
+     * @param chance {float} will determine the odds for the walker to perform a function
+     */
     public Walker(Vector2 pos, Vector2 dir, float chance)
     {
         Direction = dir;
