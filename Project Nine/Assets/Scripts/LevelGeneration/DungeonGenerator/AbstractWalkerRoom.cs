4afc0588374cf8634daae8820ba3274ae3c1e397
Aedan <adefra5@lsu.edu>
2025-03-14 22:53:23 -0500

    [feat] created and staged a new class WalkerRoom1x1 : AbstractWalkerRoom

--- Project Nine/Assets/Scripts/LevelGeneration/DungeonGenerator/AbstractWalkerRoom.cs
+++ Project Nine/Assets/Scripts/LevelGeneration/DungeonGenerator/AbstractWalkerRoom.cs
@@ -11,6 +11,69 @@ public abstract class AbstractWalkerRoom : AbstractRoom
 
     protected Grid[,] roomGrid = new Grid[32, 32];
 
+    protected override void InitializeRoom()
+    {
+        for (int i = 0; i < roomGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < roomGrid.GetLength(1); j++)
+            {
+                roomGrid[i, j] = Grid.EMPTY;
+            }
+        }
+        
+        Vector2Int walkerPos = new Vector2Int(roomGrid.GetLength(0) / 2, roomGrid.GetLength(1) / 2);
+        
+        Walker curWalker = new Walker(walkerPos, GetDirection(), 0.5f);
+        walkerList.Add(curWalker);
+        roomGrid[walkerPos.x, walkerPos.y] = Grid.FLOOR;
+
+        
+        floorCount++;
+    }
+    protected override void GenerateWalls()
+    {
+        for (int i = 0; i < roomGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < roomGrid.GetLength(1); j++)
+            {
+                if (roomGrid[i, j] == Grid.FLOOR)
+                {
+                    if (roomGrid[i + 1, j] == Grid.EMPTY)
+                    {
+                        roomGrid[i + 1, j] = Grid.WALL;
+                    }
+                    if (roomGrid[i - 1, j] == Grid.EMPTY)
+                    {
+                        roomGrid[i - 1, j] = Grid.WALL;
+                    }
+                    if (roomGrid[i, j + 1] == Grid.EMPTY)
+                    {
+                        roomGrid[i, j + 1] = Grid.WALL;
+                    }
+                    if (roomGrid[i, j - 1] == Grid.EMPTY)
+                    {
+                        roomGrid[i, j - 1] = Grid.WALL;
+                    }
+                    if (roomGrid[i + 1, j + 1] == Grid.EMPTY)
+                    {
+                        roomGrid[i + 1, j + 1] = Grid.WALL;
+                    }
+                    if (roomGrid[i - 1, j - 1] == Grid.EMPTY)
+                    {
+                        roomGrid[i - 1, j - 1] = Grid.WALL;
+                    }
+                    if (roomGrid[i - 1, j + 1] == Grid.EMPTY)
+                    {
+                        roomGrid[i - 1, j + 1] = Grid.WALL;
+                    }
+                    if (roomGrid[i + 1, j - 1] == Grid.EMPTY)
+                    {
+                        roomGrid[i + 1, j - 1] = Grid.WALL;
+                    }
+                }
+            }
+        }
+    }
     protected abstract void RemoveWalker();
     protected abstract void ChangeDirection();
     protected abstract void DuplicateWalker();
---
+++ Project Nine/Assets/Scripts/LevelGeneration/DungeonGenerator/WalkerRoom1x1.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class WalkerRoom1x1 : AbstractWalkerRoom
+{
+    public WalkerRoom1x1()
+    {
+
+    }
+
+    protected override void GenerateFloor()
+    {
+
+    }
+
+    protected override void RemoveWalker()
+    {
+
+    }
+
+    protected override void ChangeDirection()
+    {
+
+    }
+    protected override void DuplicateWalker()
+    {
+
+    }
+    protected override void UpdateWalkerPosition()
+    {
+
+    }
+}
