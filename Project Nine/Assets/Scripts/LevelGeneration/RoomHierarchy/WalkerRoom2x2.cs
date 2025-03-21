552bc05e79216e435d52cc30bca4a1ea361dd8e5
Aedan <adefra5@lsu.edu>
2025-03-21 18:41:08 -0500

    [feat] added new room generation calss, WalkerRoom2x2, that generates smoother more full rooms with the walker

---
+++ Project Nine/Assets/Scripts/LevelGeneration/RoomHierarchy/WalkerRoom2x2.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class WalkerRoom2x2 : AbstractWalkerRoom
+{
+    public WalkerRoom2x2()
+    {
+        InitializeRoom();
+        GenerateFloor();
+        GenerateWalls();
+    }
+    
+    protected override void GenerateFloor()
+    {
+        while ((float)floorCount / (int)roomGrid.Length < fillPercent)
+        {
+
+            foreach (Walker walker in walkerList)
+            {
+                Vector2Int newWalkerPos = new Vector2Int((int)walker.Position.x, (int)walker.Position.y);
+                if (roomGrid[newWalkerPos.x, newWalkerPos.y] != Grid.FLOOR)
+                {
+                    roomGrid[newWalkerPos.x, newWalkerPos.y] = Grid.FLOOR;
+                    floorCount++;
+                }
+            }
+            Cycle2x2();
+            RemoveWalker();
+            ChangeDirection();
+            DuplicateWalker();
+            UpdateWalkerPosition();
+        }
+    }
+    protected void Cycle2x2()
+    {
+        for (int i = 0; i < walkerList.Count; i++)
+        {
+            Walker walker = walkerList[i];
+
+            walker.Position = walker.Position + Vector2.up;
+            if (walker.Position.y < 31 && walker.Position.y > 0 && walker.Position.x < 31 && walker.Position.x > 0)
+            {
+                if (roomGrid[(int)walker.Position.x, (int)walker.Position.y] != Grid.FLOOR)
+                {
+                    roomGrid[(int)walker.Position.x, (int)walker.Position.y] = Grid.FLOOR;
+                    floorCount++;
+                }
+            }
+
+            walker.Position = walker.Position + Vector2.right;
+            if (walker.Position.y < 31 && walker.Position.y > 0 && walker.Position.x < 31 && walker.Position.x > 0)
+            {
+                if (roomGrid[(int)walker.Position.x, (int)walker.Position.y] != Grid.FLOOR)
+                {
+                    roomGrid[(int)walker.Position.x, (int)walker.Position.y] = Grid.FLOOR;
+                    floorCount++;
+                }
+            }
+
+            walker.Position = walker.Position + Vector2.down;
+            if (walker.Position.y < 31 && walker.Position.y > 0 && walker.Position.x < 31 && walker.Position.x > 0)
+            {
+                if (roomGrid[(int)walker.Position.x, (int)walker.Position.y] != Grid.FLOOR)
+                {
+                    roomGrid[(int)walker.Position.x, (int)walker.Position.y] = Grid.FLOOR;
+                    floorCount++;
+                }
+            }
+
+            walker.Position = walker.Position + Vector2.left;
+        }
+    }
+    protected override void RemoveWalker()
+    {
+        int updatedCount = walkerList.Count;
+        for (int i = 0; i < updatedCount; i++)
+        {
+            if (UnityEngine.Random.value < walkerList[i].chanceToChange && walkerList.Count > 1)
+            {
+                walkerList.RemoveAt(i);
+                break;
+            }
+        }
+    }
+    protected override void ChangeDirection()
+    {
+        for (int i = 0; i < walkerList.Count; i++)
+        {
+            if (UnityEngine.Random.value < walkerList[i].chanceToChange)
+            {
+                Walker curWalker = walkerList[i];
+                curWalker.Direction = GetDirection();
+                walkerList[i] = curWalker;
+            }
+        }
+    }
+    protected override void DuplicateWalker()
+    {
+        int updateCount = walkerList.Count;
+        for (int i = 0; i < updateCount; i++)
+        {
+            if (UnityEngine.Random.value < walkerList[i].chanceToChange && walkerList.Count < maxWalkers)
+            {
+                Vector2 newDirection = GetDirection();
+                Vector2 newPosition = walkerList[i].Position;
+                Walker newWalker = new Walker(newPosition, newDirection, 0.5f);
+                walkerList.Add(newWalker);
+            }
+        }
+    }
+    protected override void UpdateWalkerPosition()
+    {
+        for (int i = 0; i < walkerList.Count; i++)
+        {
+            Walker foundWalker = walkerList[i];
+            foundWalker.Position += foundWalker.Direction;
+            foundWalker.Position.x = Mathf.Clamp(foundWalker.Position.x, 1, roomGrid.GetLength(0) - 2);
+            foundWalker.Position.y = Mathf.Clamp(foundWalker.Position.y, 1, roomGrid.GetLength(1) - 2);
+            walkerList[i] = foundWalker;
+        }
+    }
+}
