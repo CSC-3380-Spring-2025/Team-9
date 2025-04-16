9f82ee3dfd2926b28fa6baac903186bddb11a8b4
carlosrodcor <crod143@lsu.edu>
2025-04-15 21:43:38 -0500

    spider script done

--- Project Nine/Assets/Scripts/Enemy/Spider.cs
+++ Project Nine/Assets/Scripts/Enemy/Spider.cs
@@ -1,7 +1,7 @@
-using Unity.VisualScripting;
+
 using UnityEngine;
-using UnityEngine.UIElements;
-using System;
+
+
 
 public class Spider : MortalEnemy, IDamageable<int>
 {
@@ -19,7 +19,7 @@ public class Spider : MortalEnemy, IDamageable<int>
     int generations = 2; // how many times the original spider will spawn offspring when dying 
 
 
-    int radius = 4; // distance from center of spider to spawn children
+    int radius = 2; // distance from center of spider to spawn children
     // the radius should also depend on the new size of the spider
 
 // i should do something different for the scaling factor
@@ -40,33 +40,33 @@ public class Spider : MortalEnemy, IDamageable<int>
 
         // the following will eventually be moved to a function of its own
         //Time.timeScale = 0.2f;
-        float radians = Mathf.PI / 4;
-        while (n > 0 && generations > 0)
-        {
-        //     GameObject childEmpty = new GameObject($"SpiderSon {n}");
-        // childEmpty.transform.SetParent(transform);
-        Vector3 parentPos = transform.position;  
-        Vector3 babySpiderPos =  new Vector3(parentPos.x + (radius*Mathf.Cos(radians)), parentPos.y + radius*Mathf.Sin(radians), parentPos.z);
+        // float radians = Mathf.PI / 4;
+        // while (n > 0 && generations > 0)
+        // {
+        // //     GameObject childEmpty = new GameObject($"SpiderSon {n}");
+        // // childEmpty.transform.SetParent(transform);
+        // Vector3 parentPos = transform.position;  
+        // Vector3 babySpiderPos =  new Vector3(parentPos.x + (radius*Mathf.Cos(radians)), parentPos.y + radius*Mathf.Sin(radians), parentPos.z);
       
-        Debug.Log("the transform of the empty child is: " + babySpiderPos);
-        GameObject childSpider = Instantiate(spiderPrefab, babySpiderPos , Quaternion.identity);
-        Vector3 scale = childSpider.transform.localScale;
-        childSpider.transform.localScale = new Vector3(scale.x/2f, scale.y/2f, scale.z);
-        childSpider.GetComponent<Spider>().generations = generations -1; 
-        childSpider.GetComponent<Spider>().radius = radius - 1; 
+        // Debug.Log("the transform of the empty child is: " + babySpiderPos);
+        // GameObject childSpider = Instantiate(spiderPrefab, babySpiderPos , Quaternion.identity);
+        // Vector3 scale = childSpider.transform.localScale;
+        // childSpider.transform.localScale = new Vector3(scale.x/2f, scale.y/2f, scale.z);
+        // childSpider.GetComponent<Spider>().generations = generations -1; 
+        // childSpider.GetComponent<Spider>().radius = radius - 1; 
         
-        n--;
-        radians += Mathf.PI / 2;
-        }
-        Destroy(gameObject, 5);
+        // n--;
+        // radians += Mathf.PI / 2;
+        // }
+        // Destroy(gameObject, 5);
     }
 
     void SpawnOffspringFromCode()
     {
         float radians = Random.Range(0f, 2*Mathf.PI);
-        radius = Random.Range(0f, )
+        float newradius = Random.Range(0f, radius);
         //float radians = Mathf.PI / 4;
-        while (n > 0 && generations > 0);
+        while (n > 0 && generations > 0)
         {
         //     GameObject childEmpty = new GameObject($"SpiderSon {n}");
         // childEmpty.transform.SetParent(transform);
@@ -89,30 +89,24 @@ public class Spider : MortalEnemy, IDamageable<int>
 
     void SpawnOffspring() // method using transforms. this function would then be called inside the Die method. 
     {
-        foreach (Transform babySpiderPos in childrenSpawnPoints)
+        foreach (Transform babySpiderPos in childrenSpawnPoints) // here babySpiderPos is a transform instead of a vector3.
         {
-            GameObject childSpider = Instantiate(spiderPrefab, babySpiderPos , Quaternion.identity);
+            GameObject childSpider = Instantiate(spiderPrefab, babySpiderPos.position, Quaternion.identity);
             Vector3 scale = childSpider.transform.localScale;
             childSpider.transform.localScale = new Vector3(scale.x/2f, scale.y/2f, scale.z);
             childSpider.GetComponent<Spider>().generations = generations -1; 
-            childSpider.GetComponent<Spider>().radius = radius - 1; 
-
+            //childSpider.GetComponent<Spider>().radius = radius - 1; 
         }
-
-
     }
 
 
     override public void Die() 
     {
-
-
         if (isDead) return;
         isDead = true;
         //before destroying, cease all animations, activity, etc.
-        
-
-        Destroy(gameObject, 20); // this might be able to go in the beginning
-
+      
+        SpawnOffspringFromCode();
+        Destroy(gameObject);
     }
 }
