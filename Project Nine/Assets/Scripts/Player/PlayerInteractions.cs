d044ecf7abbfca73300913bc39054a6d734b649a
Aedan <adefra5@lsu.edu>
2025-04-18 12:43:09 -0500

    [feat] Added new prefab for the Bow Item, and set up the interaction with the player character. I also created some folders and added files to them where I saw fit, for the sake of organization.

--- Project Nine/Assets/Scripts/Player/PlayerInteractions.cs
+++ Project Nine/Assets/Scripts/Player/PlayerInteractions.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 public class PlayerInteractions : MonoBehaviour
 {
     private Door nearbyDoor;
+    private BowInteraction nearbyBow;
     void Update()
     {
-        if(nearbyDoor != null && Input.GetKeyDown(KeyCode.E))
+        if (nearbyBow != null && Input.GetKeyDown(KeyCode.E))
+        {
+            nearbyBow.Interact();
+        }
+        if (nearbyDoor != null && Input.GetKeyDown(KeyCode.E))
         {
             nearbyDoor.Interact();
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Bow"))
+        {
+            nearbyBow = collision.GetComponent<BowInteraction>();
+        }
         if (collision.CompareTag("Door"))
         {
             nearbyDoor = collision.GetComponent<Door>();
@@ -20,6 +29,13 @@ public class PlayerInteractions : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (collision.CompareTag("Door"))
+        {
+            if(nearbyBow == collision.GetComponent<BowInteraction>())
+            {
+                nearbyBow = null;
+            }
+        }
         if(collision.CompareTag("Door"))
         {
             if(nearbyDoor == collision.GetComponent<Door>())
